79af096bd34af493f7a6b07b5aa4426ab12cec95
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 21:33:41 +0300

    Scripted auto-commit on change (2019-11-15 21:33:40) by gitwatch.sh

--- src/main/java/lab5/Server.java
+++ src/main/java/lab5/Server.java
@@ -1,50 +1,76 @@
 package lab5;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.japi.Pair;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
 import akka.util.ByteString;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.concurrent.CompletionStage;
 
 public class Server {
+
     public static void main(String[] args) throws IOException {
         /*а. Инициализация http сервера в akka */
-        ActorSystem system = ActorSystem.create("routes");
+        ActorSystem system = ActorSystem.create("JSServerActorSystem");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        final CompletionStage<ServerBinding> binding = Http.get(system).bindAndHandle(
-                request -> {
-                    if (request.getUri().path().equals("/")) {
-                        String testURL = request.getUri().query().get("testURL").orElse("");
-                        String count = request.getUri().query().get("count").orElse("");
-                        if (testURL.isEmpty()) {
-                            return HttpResponse.create().withEntity(ByteString.fromString("testURL is empty"));
-                        } else if (count.isEmpty()) {
-                            return HttpResponse.create().withEntity(ByteString.fromString("count is empty"));
-                        } else {
-                            try {
-                            } catch (NumberFormatException e) {
-                                return HttpResponse.create().withEntity(ByteString.fromString("count is not an integer"));
-                            }
-                        }
-                    } else {
-                        return HttpResponse.create().withEntity(ByteString.fromString("404"));
-                    }
-                },
-                ConnectHttp.toHost("localhost", 8080),
-                materializer
-        );
+        CompletionStage<ServerBinding> serverBindingFuture =
+                Http.get(system).bindAndHandleSync(
+                        request -> {
+                            if (request.getUri().path().equals("/")) {
+                                String testURL = request.getUri().query().get("testURL").orElse("");
+                                String count = request.getUri().query().get("count").orElse("");
+                                if (testURL.isEmpty()) {
+                                    return HttpResponse.create().withEntity(ByteString.fromString("testURL is empty"));
+                                } else if (count.isEmpty()) {
+                                    return HttpResponse.create().withEntity(ByteString.fromString("count is empty"));
+                                } else {
+                                    try {
+                                        Integer countInteger = Integer.parseInt(count);
+                                        System.out.println("testURL = " + testURL);
+                                        System.out.println("count = " + countInteger);
+                                        Pair<String, Integer> input = new Pair<>(testURL, countInteger);
+                                        Source<Pair<String, Integer>, NotUsed> source = Source.from(Collections.singletonList(input));
+                                        /*б. Общая логика требуемого flow*/
+                                        Flow<Pair<String, Integer>, HttpResponse, NotUsed> flow = Flow.<Pair<String, Integer>>create()
+                                                /*map в Pair<HttpRequest этот запрос создан из query параметра, Integer количество запросов> */
+                                                .map(pair -> new Pair<>(HttpRequest.create().withUri(pair.first()), pair.second()))
+                                                /*mapAsync, создаем на лету flow из данных запроса, выполняем его и возвращаем*/
+                                                .mapAsync(1, pair -> {
+                                                })
 
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+                                        CompletionStage<HttpResponse> result = source.via(flow).toMat(Sink.last(), Keep.right()).run(materializer);
+                                        return result.toCompletableFuture().get();
+                                    } catch (NumberFormatException e) {
+                                        return HttpResponse.create().withEntity(ByteString.fromString("count is not an integer"));
+                                    }
+                                }
+                            } else {
+                                return HttpResponse.create().withEntity(ByteString.fromString("404"));
+                            }
+                        }, ConnectHttp.toHost("localhost", 8083), materializer);
+
+        System.out.println("Server online at http://localhost:8083/\nPress RETURN to stop...");
         System.in.read();
-        binding
-                .thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> system.terminate());
+
+        serverBindingFuture
+                .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+
     }
 }
+
+
