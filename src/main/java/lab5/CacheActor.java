aae9278c121a1a37e59e89f2cbb61f7de7c824eb
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 21:45:46 +0300

    Scripted auto-commit on change (2019-11-15 21:45:46) by gitwatch.sh

--- src/main/java/lab5/CacheActor.java
+++ src/main/java/lab5/CacheActor.java
@@ -1,4 +1,30 @@
-package lab5;
+package lab4;
 
-public class CacheActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.*;
+
+public class CacheActor extends AbstractActor {
+    /*б. В приложении будем использовать следующие акторы :
+    - актор который хранит результаты тестов.
+   Обрабатывает следующие сообщения :
+   cообщение с результатом одного теста -> кладет его в локальное хранилище.
+   Сообщение с запросом результата теста → отвечает сообщением с  результатом всех тестов для заданного  packageId */
+    private Map<String, Map<String, String>> storage = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    if (!storage.containsKey(m.getPackageId())) {
+                        storage.put(m.getPackageId(), new HashMap<>());
+                    }
+                    storage.get(m.getPackageId()).put(m.getTestName(), m.getResult());
+                    System.out.println("Message for if:" + m.getPackageId() + "received");
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new FullAnswer(req.getPackageId(), storage.get(req.getPackageId())), self())
+                ).build();
+    }
 }
--- src/main/java/lab5/GetMessage.java
+++ src/main/java/lab5/GetMessage.java
@@ -1,4 +1,20 @@
 package lab5;
 
 public class GetMessage {
+    private String site;
+    private int requestCount;
+
+    public GetMessage(String site, int requestCount) {
+        this.site = site;
+        this.requestCount = requestCount;
+    }
+
+    public int getRequestCount() {
+        return requestCount;
+    }
+
+    public String getSite() {
+        return site;
+    }
 }
+
