25175bb8c7e4b77b691a34a993c0898fe5379a30
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 21:25:02 +0300

    Scripted auto-commit on change (2019-11-15 21:25:01) by gitwatch.sh

--- src/main/java/lab5/Server.java
+++ src/main/java/lab5/Server.java
@@ -1,14 +1,11 @@
 package lab5;
 
-import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
 import akka.util.ByteString;
 
 import java.io.IOException;
@@ -21,27 +18,25 @@ public class Server {
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = request -> {
-            if (request.getUri().path().equals("/")) {
-                String testURL = request.getUri().query().get("testURL").orElse("");
-                String count = request.getUri().query().get("count").orElse("");
-                if (testURL.isEmpty()) {
-                    return HttpResponse.create().withEntity(ByteString.fromString("testURL is empty"));
-                } else if (count.isEmpty()) {
-                    return HttpResponse.create().withEntity(ByteString.fromString("count is empty"));
-                } else {
-                    try {
-                    } catch (NumberFormatException e) {
-                        return HttpResponse.create().withEntity(ByteString.fromString("count is not an integer"));
-                    }
-                }
-            } else {
-                return HttpResponse.create().withEntity(ByteString.fromString("404"));
-            }
-        };
-
         final CompletionStage<ServerBinding> binding = Http.get(system).bindAndHandle(
-                routeFlow,
+                request -> {
+                    if (request.getUri().path().equals("/")) {
+                        String testURL = request.getUri().query().get("testURL").orElse("");
+                        String count = request.getUri().query().get("count").orElse("");
+                        if (testURL.isEmpty()) {
+                            return HttpResponse.create().withEntity(ByteString.fromString("testURL is empty"));
+                        } else if (count.isEmpty()) {
+                            return HttpResponse.create().withEntity(ByteString.fromString("count is empty"));
+                        } else {
+                            try {
+                            } catch (NumberFormatException e) {
+                                return HttpResponse.create().withEntity(ByteString.fromString("count is not an integer"));
+                            }
+                        }
+                    } else {
+                        return HttpResponse.create().withEntity(ByteString.fromString("404"));
+                    }
+                },
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
