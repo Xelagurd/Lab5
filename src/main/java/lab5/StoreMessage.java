ab1557e24fa23264cc17e9af25e21f00231d0410
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 21:47:06 +0300

    Scripted auto-commit on change (2019-11-15 21:47:05) by gitwatch.sh

--- src/main/java/lab5/StoreMessage.java
+++ src/main/java/lab5/StoreMessage.java
@@ -1,4 +1,26 @@
 package lab5;
 
 public class StoreMessage {
+    private String site;
+    private int requestCount;
+    private String result;
+
+    public StoreMessage(String site, int requestCount,String result) {
+        this.site = site;
+        this.requestCount = requestCount;
+        this.result = result;
+    }
+
+    public int getRequestCount() {
+        return requestCount;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
+
