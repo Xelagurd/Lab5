e8b7fd987732925dc7b6256fec50769a8ebf4b4c
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 20:46:55 +0300

    Scripted auto-commit on change (2019-11-15 20:46:55) by gitwatch.sh

--- src/main/java/lab5/Server.java
+++ src/main/java/lab5/Server.java
@@ -1,3 +1,9 @@
+package lab5;
+
+import java.io.IOException;
+
 public class Server {
-      
+    public static void main(String[] args) throws IOException {
+        /*а. Инициализация http сервера в akka */
+    }
 }
